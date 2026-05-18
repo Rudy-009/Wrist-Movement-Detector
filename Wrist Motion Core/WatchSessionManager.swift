9ae96c8b3736c3afec8d4c979f7fc4c4556f97f9
Rudy-009 <rofxnaos@gmail.com>
2026-05-18 22:30:10 +0900

    [feat] WCSession 관리 객체 생성

---
+++ Wrist Motion Core/WatchSessionManager.swift
@@ -0,0 +1,58 @@
+//
+//  WatchSessionManager.swift
+//  Wrist Motion
+//
+//  Created by Seungjun Lee on 5/18/26.
+//
+
+import WatchConnectivity
+
+@Observable
+final class WatchSessionManager: NSObject {
+    
+    var state = WCSessionActivationState.notActivated
+    var isRechable: Bool = false
+    
+    override init() {
+        super.init()
+        guard WCSession.isSupported() else {
+            fatalError("Watch Connectivity가 지원되지 않는 기기입니다.")
+        }
+        WCSession.default.delegate = self
+        WCSession.default.activate()
+        // let _ = WCSession.default.activationState
+    }
+}
+
+extension WatchSessionManager: WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: (any Error)?) {
+        state = WCSession.default.activationState
+    }
+    
+    #if os(iOS)
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        // 세션이 비활성 상태로 전환될 때 (예: 다른 Apple Watch로 전환 중)
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        // 세션이 완전히 비활성화됐을 때
+        // 재활성
+        WCSession.default.activate()
+        // 비활성 알림
+    }
+    #endif
+    
+    func sessionReachabilityDidChange(_ session: WCSession) {
+        isRechable = WCSession.default.isReachable
+    }
+}
+
+extension WatchSessionManager {
+    func sendFile(file: URL, metadata: [String : Any]?) {
+        WCSession.default.transferFile(file, metadata: metadata)
+    }
+    
+    func session(_ session: WCSession, didFinish fileTransfer: WCSessionFileTransfer, error: (any Error)?) {
+        
+    }
+}
